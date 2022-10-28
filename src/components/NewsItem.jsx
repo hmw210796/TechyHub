e06d1e1bbd4865d85e6558429c7824d471bcb422
Ming Wei <hmw210796@hotmail.com>
2022-10-28 11:09:14 +0800

    first commit

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,23 +2,25 @@ import { Link } from "react-router-dom";
 
 function NewsItem(props) {
   return (
-    <div className="m-2 w-full shadow-lg rounded-md">
+    <div className="m-2 w-full shadow-lg rounded-xl container">
       <div className="relative container">
         <img
           src="/src/image/reading.png"
           alt="reading"
-          className="object-fill"
+          className="object-fill rounded-t-lg"
         />
         <div className="absolute bottom-3 right-4  bg-blue-900/80 rounded-2xl w-16 p-1 ">
           <div className="font-bold text-center text-xs text-white">News</div>
         </div>
       </div>
 
-      <div className="p-5">
-        <h1 className="font-bold text-xl line-clamp-2">{props.title}</h1>
-        <h2 className="font-thin text-gray-400 text-xs pt-2 line-clamp-2">
-          {props.body}
-        </h2>
+      <div className="p-5 flex flex-col justify-between h-44 container">
+        <div>
+          <h1 className="font-bold text-xl line-clamp-2">{props.title}</h1>
+          <h2 className="font-thin text-gray-400 text-xs pt-2 line-clamp-2">
+            {props.body}
+          </h2>
+        </div>
         <div className="pt-2">
           <Link to={{ pathname: `post/${props.id}` }}>
             {" "}
